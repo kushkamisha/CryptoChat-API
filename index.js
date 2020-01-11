307462c379f1d0f7fd8b853cf497fd006b618165
Misha Kushka <kushka.misha@gmail.com>
2020-01-11 14:20:07 +0200

    Start refactoring the code...

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ const abi = require('ethereumjs-abi')
 const path = require('path')
 const rlp = require('rlp')
 const txDecoder = require('ethereum-tx-decoder')
+const Contract = require('./smart-contract')
+const contract = new Contract('abi.json', '0x24FbBB41074552a8Daea6a574A01F3324A8c11BA')
 
 // Create an address
-const { address, privateKey, signTransaction, sign, encrypt } = web3.eth.accounts.create()
-console.log({ address, privateKey, signTransaction, sign, encrypt });
+// const { address, privateKey, signTransaction, sign, encrypt } = web3.eth.accounts.create()
+// console.log({ address, privateKey, signTransaction, sign, encrypt });
 
-(async () => {
-    const contractAddress = '0x24FbBB41074552a8Daea6a574A01F3324A8c11BA'
-    const contractAbi = JSON.parse(fs.readFileSync(path.join(__dirname, 'abi.json')))
+    // const contractAddress = '0x24FbBB41074552a8Daea6a574A01F3324A8c11BA'
+    // const contractAbi = JSON.parse(fs.readFileSync(path.join(__dirname, 'abi.json')))
 
-    const to = '0xbc99f9A7A24252239D318C31De697e38635566E5'
-    const amount = web3.utils.toWei('0.1', 'ether')
-    const gas = await web3.eth.getGasPrice()
+    // const to = '0xbc99f9A7A24252239D318C31De697e38635566E5'
+    // const amount = web3.utils.toWei('0.1', 'ether')
+    // const gas = await web3.eth.getGasPrice()
     // const data = '' //
     
-    const contract = new web3.eth.Contract(contractAbi, contractAddress)
+    // const contract = new web3.eth.Contract(contractAbi, contractAddress)
     // send(
     //     contract,
     //     '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF',
@@ -38,61 +39,99 @@ console.log({ address, privateKey, signTransaction, sign, encrypt });
     //         // showContractBalances()
     //     })
 
-    transfer(
-        '0xbc99f9A7A24252239D318C31De697e38635566E5',
-        web3.utils.toWei('0.05', 'ether'),
-        contract,
-        '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF',
-        '150a4d7cec81df9f8da1625e02a73ff51b827419bad7b29b744201577ddff8d3'
-        )
-        .then(tx => {
-            console.log(tx)
-
-            const decodedTx = txDecoder.decodeTx(tx.rawTransaction)
-            console.log(decodedTx)
-
-            const data = decodedTx.data.slice(10)
-            console.log(data)
-
-            const decoded = abi.rawDecode(['address', 'uint256'], Buffer.from(data, 'hex'))
-            console.log(decoded)
-            console.log(`Recipient address: 0x${decoded[0]}`)
-            console.log(`Sent amount: ${web3.utils.fromWei(decoded[1])} Ether`)
-
-            console.log(`Is the signer address correct? ${verifySigner(
-                    '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF', tx) ?
-                    'yes' : 'no'
-                }`)
-
-            // showContractBalances()
-        })
-
-    // withdraw(
-    //     web3.utils.toWei('1', 'ether'),
+    // transfer(
+    //     '0xbc99f9A7A24252239D318C31De697e38635566E5',
+    //     web3.utils.toWei('0.05', 'ether'),
     //     contract,
     //     '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF',
     //     '150a4d7cec81df9f8da1625e02a73ff51b827419bad7b29b744201577ddff8d3'
-    // ).then(console.log)
-    
+    //     )
+    //     .then(tx => {
+    //         console.log(tx)
 
-    // const tx = {
-    //     to,
-    //     value: web3.utils.toHex(amount),
-    //     // gas: 200000,
-    //     // data,
-    //     gas,
-    //     chainId: 3
-    // }
-    // const signature = await web3.eth.accounts.signTransaction(tx, privateKey)
-    // console.log({ signature })
+    //         const decodedTx = txDecoder.decodeTx(tx.rawTransaction)
+    //         console.log(decodedTx)
 
-})()
+    //         const data = decodedTx.data.slice(10)
+    //         console.log(data)
+
+    //         const decoded = abi.rawDecode(['address', 'uint256'], Buffer.from(data, 'hex'))
+    //         console.log(decoded)
+    //         console.log(`Recipient address: 0x${decoded[0]}`)
+    //         console.log(`Sent amount: ${web3.utils.fromWei(decoded[1])} Ether`)
+
+    //         console.log(`Is the signer address correct? ${verifySigner(
+    //                 '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF', tx) ?
+    //                 'yes' : 'no'
+    //             }`)
+
+    //         // showContractBalances()
+    //     })
+
+contract.withdraw(
+    '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF',
+    '150a4d7cec81df9f8da1625e02a73ff51b827419bad7b29b744201577ddff8d3',
+    web3.utils.toWei('1', 'ether')
+).then(tx => {
+    console.log(tx)
+
+    const decodedTx = txDecoder.decodeTx(tx.rawTransaction)
+    console.log({ decodedTx })
+
+    let data = decodedTx.data
+    console.log({ data })
+    data = data.slice(10)
+    console.log({ data })
+
+    const decoded = abi.rawDecode(['uint256'], Buffer.from(data, 'hex'))
+    console.log({ decoded })
+    console.log(`Withdrawal amount: ${web3.utils.fromWei(decoded[0])} Ether`)
+
+    console.log(`Is the signer address correct? ${verifySigner(
+        '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF', tx) ?
+        'yes' : 'no'
+        }`)
+
+    // web3.eth.sendSignedTransaction(tx.rawTransaction)
+})
+
+let address = '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF'
+contract.balanceOf(address)
+    .then(bal => 
+        console.log(`Balance of ${address} is ${web3.utils.fromWei(bal)} Ether`))
+
+address = '0xbc99f9A7A24252239D318C31De697e38635566E5'
+contract.balanceOf(address)
+    .then(bal =>
+        console.log(`Balance of ${address} is ${web3.utils.fromWei(bal)} Ether`))
+
+// showContractBalances()
+
+// contract.deposit(
+//     '0xbc99f9A7A24252239D318C31De697e38635566E5',
+//     '3711613e3e796754e50c0bc1be237ce603600ec7b77744f72b60a284690f1c29',
+//     web3.utils.toWei('1', 'ether')
+// ).then(tx => {
+//     console.log(tx)
+
+//     // web3.eth.sendSignedTransaction(tx.rawTransaction)
+// })
+
+// contract.transfer(
+//     '0xbc99f9A7A24252239D318C31De697e38635566E5',
+//     '3711613e3e796754e50c0bc1be237ce603600ec7b77744f72b60a284690f1c29',
+//     '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF',
+//     web3.utils.toWei('8', 'ether')
+// ).then(tx => {
+//     console.log(tx)
+//     web3.eth.sendSignedTransaction(tx.rawTransaction)
+// })
 
 function showContractBalances() {
-    contract.methods.balanceOf('0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF').call()
+    contract.instance().methods.balanceOf('0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF').call()
         .then(res => console.log(`Balance of '0xBd670c480DE5F6FE13a649dbc13Ed52A33251edF': ${res}`))
 
-    contract.methods.balanceOf('0xbc99f9A7A24252239D318C31De697e38635566E5').call()
+    contract.instance().methods.balanceOf('0xbc99f9A7A24252239D318C31De697e38635566E5').call()
         .then(res => console.log(`Balance of '0xbc99f9A7A24252239D318C31De697e38635566E5': ${res}`))
 }
 
@@ -105,55 +144,4 @@ function verifySigner(address, tx) {
     })
 
     return signer === address
-}
-
-function send(contract, sender, value, privateKey) {
-    const query = contract.methods.deposit(/* params */)
-    const encodedABI = query.encodeABI()
-    return web3.eth.accounts.signTransaction(
-        {
-            nonce: web3.eth.getTransactionCount(sender),
-            data: encodedABI,
-            from: sender,
-            gas: 2000000,
-            to: contract.options.address,
-            value
-        },
-        privateKey,
-        false,
-    )
-}
-
-function transfer(recipient, amount, contract, sender, privateKey) {
-    const query = contract.methods.transfer(recipient, amount)
-    console.log({ query })
-    const encodedABI = query.encodeABI()
-    console.log({ encodedABI })
-    return web3.eth.accounts.signTransaction(
-        {
-            nonce: web3.eth.getTransactionCount(sender),
-            data: encodedABI,
-            from: sender,
-            gas: 2000000,
-            to: contract.options.address
-        },
-        privateKey,
-        false,
-    )
-}
-
-function withdraw(amount, contract, sender, privateKey) {
-    const query = contract.methods.withdraw(amount)
-    const encodedABI = query.encodeABI()
-    return web3.eth.accounts.signTransaction(
-        {
-            nonce: web3.eth.getTransactionCount(sender),
-            data: encodedABI,
-            from: sender,
-            gas: 2000000,
-            to: contract.options.address
-        },
-        privateKey,
-        false,
-    )
 }
---
+++ smart-contract.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
+
+module.exports = class Contract {
+
+    constructor(abiFilename, address) {
+        this.abi = JSON.parse(
+            fs.readFileSync(path.join(__dirname, abiFilename)))
+        this.address = address
+        this.contract = new web3.eth.Contract(this.abi, this.address)
+    }
+
+    instance() { return this.contract }
+
+    balanceOf(address) {
+        return this.contract.methods.balanceOf(address).call()
+    }
+
+    deposit(sender, privateKey, value) {
+        return this.__send(sender, privateKey, 'deposit', [], value)
+    }
+
+    transfer(sender, privateKey, recipient, amount) {
+        return this.__send(sender, privateKey, 'transfer', [recipient, amount])
+    }
+
+    // deposit(sender, privateKey, ) {
+    //     return this.__send(sender, privateKey, 'deposit', [], value)
+    // }
+
+    withdraw(sender, privateKey, amount) {
+        return this.__send(sender, privateKey, 'withdraw', [amount])
+    }
+
+    __send(sender, privateKey, method, params, value = 0) {
+        const query = this.contract.methods[method](...params)
+        const encodedABI = query.encodeABI()
+
+        return web3.eth.accounts.signTransaction(
+            {
+                nonce: web3.eth.getTransactionCount(sender),
+                data: encodedABI,
+                from: sender,
+                gas: 2000000,
+                to: this.contract.options.address,
+                value
+            },
+            privateKey,
+            false,
+        )
+    }
+}
