34658cb4111d05e232e7efe5952c39201b1facc9
Misha Kushka <kushka.misha@gmail.com>
2021-02-01 07:49:44 +0200

    Add port env  variable for Heroku

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
-const config = require('./config')
 
-const port = config.port || 3000
+const port = process.env.PORT || 3000
 const app = express()
 const server = require('http').createServer(app)
 // const io = new (require('./utils/socket'))(server)
